14313f3d701841c85990af7c9b549ccefa9d2b1f
olrlozl <thakd221@naver.com>
2024-06-14 21:41:58 +0900

    모달창 외부 클릭 시 모달 닫게 만드는 Custom Hooks 생성

--- src/components/MovieModal/index.js
+++ src/components/MovieModal/index.js
@@ -1,4 +1,5 @@
 import React, { useRef } from 'react'
+import useOnClickOutside from '../../hooks/useOnClickOutside';
 import "./MovieModal.css";
 
 const MovieModal = ({
@@ -12,10 +13,17 @@ const MovieModal = ({
     setModalOpen
 }) => {
 
+    const ref = useRef();
+    console.log('ref', ref.current);
+
+    useOnClickOutside(ref, () => {
+        setModalOpen(false);
+    })
+
     return (
         <div className='presentation' role="presentation">
             <div className='wrapper-modal'>
-                <div className='modal'>
+                <div className='modal' ref={ref}>
                     <span
                         onClick={() => setModalOpen(false)}
                         className="modal-close"
