d7a337a40344409932fcf748de7f7defcc3ecb0c
olrlozl <thakd221@naver.com>
2024-06-14 21:04:26 +0900

    영화 나열을 위한 Row 컴포넌트 생성

--- src/pages/MainPage/index.js
+++ src/pages/MainPage/index.js
@@ -12,6 +12,10 @@ const MainPage = () => {
       <Nav />
       <Banner />
       <Category />
+      <Row title="Trending Now" id="TN" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" id="TR" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" id="AM" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" id="CM" fetchUrl={requests.fetchComedyMovies} />
     </Container>
   )
 }
